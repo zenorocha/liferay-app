fa8f6e2292665a08fb08e2dcc66aeb517750ced4
Maira Bello <maira.araujo@liferay.com>
2014-08-28 15:29:17 -0300

    Making markdown, translations and other files work with watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,14 +1,33 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
+var path = require('path');
 var config = require('./lib/ProductFlavors').generateFlavoredConfig();
 
 gulp.task('watch', ['serve'], function(cb) {
+  gulp.watch('src/**', function(event) {
+    var distPath = path.join(
+      'dist',
+      path.relative(path.join(process.cwd(), 'src'), event.path)
+    );
+
+    if (event.type == 'deleted') {
+      fs.unlink(distPath);
+    }
+    else {
+      gulp.src(event.path)
+        .pipe(gulp.dest(path.dirname(distPath)));
+    }
+  });
+
   gulp.watch(config.globHtml, ['build-html']);
   gulp.watch(config.globIcon, ['build-icons']);
   gulp.watch(config.globImage, ['build-images']);
+  gulp.watch(config.globMarkdown, ['build-markdown']);
   gulp.watch(config.globScript, ['build-scripts']);
   gulp.watch(config.globScss, ['build-compass']);
   gulp.watch(config.globStyle, ['build-styles']);
   gulp.watch(config.globTemplate, ['build-templates']);
+  gulp.watch(config.translationsFilepath.replace('{LOCALE}', '*'), ['build-templates']);
 });
