8d1cd38a51a0b3110d9c29530eca4bc84bfea279
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-09-11 11:28:34 +0200

    Separates modules into dependencies - Fixes #74

--- tasks/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-
-gulp.task('test', function() {
-  return gulp.src('test/unit/**/*.js', '!test/unit/**/fixture/**/*.js')
-    .pipe(plugins.mocha());
-});
